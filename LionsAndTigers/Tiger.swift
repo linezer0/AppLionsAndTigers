b19e11f5ef293b1e2778a1c95222ed5ce9168745
Thomas Parker <thomasparker@MacBook-Pro-de-Thomas.local>
2014-11-11 07:10:55 +0100

    First commit

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Thomas Parker on 09/11/2014.
+//  Copyright (c) 2014 Thomas Parker. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var name = ""
+    var age = 0
+    var breed = ""
+    var image = UIImage(named: "")
+}
